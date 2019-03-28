3b54b091de5c43449d154666048e86b5db293eba
Chungsang Tom Lam <erinata@gmail.com>
2019-03-28 16:43:22 -0400

    try moon data

--- run_kmc_moon.py
+++ run_kmc_moon.py
@@ -1,18 +1,18 @@
-from sklearn.datasets.samples_generator import make_blobs
+from sklearn.datasets.samples_generator import make_moons
 import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.cluster import KMeans
 
-X,Y = make_blobs(n_samples=400, centers=4, 
-	cluster_std=0.60, random_state=0)
+X,Y = make_moons(n_samples=400, noise =0.05, random_state=0)
 
 # print(X)
 
 plt.scatter(X[:,0], X[:,1])
 plt.savefig('scatterplot.png')
 
-kmeans = KMeans(n_clusters=7)
+
+kmeans = KMeans(n_clusters=2)
 kmeans.fit(X)
 kmeans_results = kmeans.predict(X)
 
