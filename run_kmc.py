0512e8143ea43d53cdfd30e4b0a6edb326ec2e24
Chungsang Tom Lam <erinata@gmail.com>
2019-04-02 13:42:57 -0400

    more variations

--- run_kmc.py
+++ run_kmc.py
@@ -5,21 +5,24 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
 X,Y = make_blobs(n_samples=400, centers=4, 
-	cluster_std=0.60, random_state=0)
+	cluster_std=0.80, random_state=0)
 
 # print(X)
 
 plt.scatter(X[:,0], X[:,1])
 plt.savefig('scatterplot.png')
 
-kmeans = KMeans(n_clusters=7)
-kmeans.fit(X)
-kmeans_results = kmeans.predict(X)
+def run_kmeans(n):
+	kmeans = KMeans(n_clusters=n)
+	kmeans.fit(X)
+	kmeans_results = kmeans.predict(X)
 
-print(kmeans_results)
+	print(kmeans_results)
 
-plt.scatter(X[:, 0], X[:, 1], c=kmeans_results)
-plt.savefig('scatterplot_color.png')
+	plt.scatter(X[:, 0], X[:, 1], c=kmeans_results)
+	plt.savefig('scatterplot_color.png')
+
+run_kmeans(4)
 
 
 
